ebc5e5a1aba3569a453eb2622541a4029eb9b158
ckhbang01-arch <ckhbang01@gmail.com>
2025-08-17 05:42:52 -0700

    Add files via upload

---
+++ words.js
@@ -0,0 +1,26 @@
+const wordData = {
+    "Gia đình": [
+        { hanViet: "Gia đình", pinyin: "Jiā tíng", hanzi: "家庭", vietnamese: "Gia đình", example: "我的家庭很幸福. (Gia đình tôi rất hạnh phúc.)" },
+        { hanViet: "Phụ thân", pinyin: "Fù qīn", hanzi: "父親", vietnamese: "Bố", example: "我的父親是一位老師. (Bố tôi là một giáo viên.)" },
+        { hanViet: "Mẫu thân", pinyin: "Mǔ qīn", hanzi: "母親", vietnamese: "Mẹ", example: "我的母親很溫柔. (Mẹ tôi rất hiền dịu.)" },
+        { hanViet: "Huynh đệ", pinyin: "Xiōng dì", hanzi: "兄弟", vietnamese: "Anh em trai", example: "他們是親兄弟. (Họ là anh em ruột.)" },
+        { hanViet: "Tỷ muội", pinyin: "Jiě mèi", hanzi: "姐妹", vietnamese: "Chị em gái", example: "我們是好姐妹. (Chúng tôi là chị em tốt.)" },
+        { hanViet: "Trưởng tử", pinyin: "Zhǎng zǐ", hanzi: "長子", vietnamese: "Con trai cả", example: "他是家裡的長子. (Anh ấy là con trai cả trong nhà.)" },
+        { hanViet: "Vợ chồng", pinyin: "Fū qī", hanzi: "夫妻", vietnamese: "Vợ chồng", example: "他們是一對恩愛的夫妻. (Họ là một cặp vợ chồng ân ái.)" },
+    ],
+    "Công việc": [
+        { hanViet: "Công tác", pinyin: "Gōng zuò", hanzi: "工作", vietnamese: "Công việc", example: "你現在的工作是什麼？ (Công việc hiện tại của bạn là gì?)" },
+        { hanViet: "Công ty", pinyin: "Gōng sī", hanzi: "公司", vietnamese: "Công ty", example: "我在一家大公司工作. (Tôi làm việc tại một công ty lớn.)" },
+        { hanViet: "Chức vụ", pinyin: "Zhí wù", hanzi: "職務", vietnamese: "Chức vụ", example: "他的職務是經理. (Chức vụ của anh ấy là giám đốc.)" },
+        { hanViet: "Thị trường", pinyin: "Shì chǎng", hanzi: "市場", vietnamese: "Thị trường", example: "這個市場競爭很激烈. (Thị trường này cạnh tranh rất khốc liệt.)" },
+        { hanViet: "Khách hàng", pinyin: "Kè hù", hanzi: "客戶", vietnamese: "Khách hàng", example: "我們需要服務好每一個客戶. (Chúng ta cần phục vụ tốt mỗi khách hàng.)" },
+    ],
+    "Trường học": [
+        { hanViet: "Học sinh", pinyin: "Xué shēng", hanzi: "學生", vietnamese: "Học sinh", example: "我是一名高中學生. (Tôi là một học sinh cấp ba.)" },
+        { hanViet: "Giáo viên", pinyin: "Jiào shī", hanzi: "教師", vietnamese: "Giáo viên", example: "他是一位很有經驗的教師. (Thầy ấy là một giáo viên rất có kinh nghiệm.)" },
+        { hanViet: "Đại học", pinyin: "Dà xué", hanzi: "大學", vietnamese: "Đại học", example: "他考上了北京大學. (Anh ấy đã thi đỗ Đại học Bắc Kinh.)" },
+{ hanViet: "Lớp học", pinyin: "Bān jí", hanzi: "班級", vietnamese: "Lớp học", example: "我們的班級很團結. (Lớp học của chúng tôi rất đoàn kết.)" },
+        { hanViet: "Giảng đường", pinyin: "Jiǎng táng", hanzi: "講堂", vietnamese: "Giảng đường", example: "這節課在A棟教學樓的講堂. (Tiết học này ở giảng đường của tòa nhà A.)" },
+    ],
+    // Thêm các chủ đề khác...
+};
